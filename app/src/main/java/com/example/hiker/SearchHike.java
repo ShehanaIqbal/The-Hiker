3e53f0da53ec51af5fc92e54ed8ac86a725e26f2
shehanai <shehanai@zone24x7.com>
2022-02-20 16:46:00 +0530

    Added the UIs

---
+++ app/src/main/java/com/example/hiker/SearchHike.java
@@ -0,0 +1,15 @@
+package com.example.hiker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchHike extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_hike);
+    }
+
+}
--- app/src/main/java/com/example/hiker/SubmitTrail.java
+++ app/src/main/java/com/example/hiker/SubmitTrail.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SubmitTrail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        goToSignUp();
+        setContentView(R.layout.activity_submit_trail);
     }
 
-    public void goToSignUp(){
-        Intent intent = new Intent(MainActivity.this, SignUp.class);
+    public void goToSignUp(View view){
+        Intent intent = new Intent(SubmitTrail.this, Home.class);
         startActivity(intent);
     }
 }
