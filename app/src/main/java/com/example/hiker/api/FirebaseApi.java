8355d855b8a3479b2c6b14afb115c8d44b1504c2
Nilaan097 <nilaan.17@cse.mrt.ac.lk>
2022-04-04 14:30:25 +0530

    added functions to save hike pictures and save comments

--- app/src/main/java/com/example/hiker/api/FirebaseApi.java
+++ app/src/main/java/com/example/hiker/api/FirebaseApi.java
@@ -1,5 +1,8 @@
 package com.example.hiker.api;
 
+import android.util.Log;
+
+import com.example.hiker.model.CommentSerializable;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -9,6 +12,7 @@ import com.google.firebase.storage.StorageReference;
 import com.example.hiker.model.Hike;
 import com.example.hiker.model.User;
 import com.example.hiker.utils.UserUtils;
+import com.google.gson.Gson;
 
 import java.util.UUID;
 
@@ -33,10 +37,22 @@ public class FirebaseApi {
         return db.collection("hikes").add(hike);
     }
 
+    public static Task<Void> saveComment(CommentSerializable comment) {
+        Log.d("FirebaseApi", new Gson().toJson(comment));
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        return db.collection("comments").document(comment.getCommentId()).set(comment);
+    }
+
     public static StorageReference saveProfilePicture() {
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReference();
         StorageReference ref = storageRef.child("profile-pics/" + UUID.randomUUID().toString());
+        return ref;
+    }
+    public static StorageReference saveHikePictures() {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference ref = storageRef.child("hike-pics/" + UUID.randomUUID().toString());
         return ref;
     }
 
