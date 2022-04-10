b306afc84ad66f30751d607d749bcb34cb1bd310
Nilaan097 <nilaan.17@cse.mrt.ac.lk>
2022-04-10 17:36:18 +0530

    loading comments ;focus to comment location on map

--- app/src/main/java/com/example/hiker/BlogActivity.java
+++ app/src/main/java/com/example/hiker/BlogActivity.java
@@ -22,6 +22,8 @@ import androidx.core.widget.ImageViewCompat;
 
 import com.example.hiker.model.CommentSerializable;
 import com.example.hiker.model.Hike;
+import com.example.hiker.model.LatLangSerializable;
+import com.example.hiker.utils.Utils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -46,9 +48,13 @@ import com.example.hiker.utils.MapperUtils;
 import com.example.hiker.utils.SharedPrefUtils;
 import com.example.hiker.utils.UserUtils;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class BlogActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
@@ -58,6 +64,8 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
     private int ZOOM_LEVEL=15;
     private HikeSerializable hike;
     private List<CommentSerializable> commentSerializable;
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,83 +89,124 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
 //            distance.setText(hike.getDistance());
 //            setLikeButtonColor(likeButton, UserUtils.userFav(user, hike.getTitle()));
             // set image
-            String[] allImageURL ={"https://static.saltinourhair.com/wp-content/uploads/2016/11/23155637/Things-to-do-Ella-Sri-Lanka-Nine-arch-bridge-view-2.jpg","https://destinationlesstravel.com/wp-content/uploads/2019/05/DSC_9600-1-1024x684-1024x684.jpg","https://destinationlesstravel.com/wp-content/uploads/2020/04/Ella-train.jpg.webp"};
-            String[] allComments = {"asd","qwe",""};
-            createHorizontalCard(hike, allImageURL, allComments);
             //new ImageLoadTask(hike.getImage(), img).execute();
 
             SupportMapFragment mapFragment =
                     (SupportMapFragment) getSupportFragmentManager()
                             .findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
-//
-//            likeButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    if (user != null) {
-//                        if (!UserUtils.userFav(user, hike.getTitle())) {
-//                            FirebaseApi.saveUserLike(hike.getTitle(), user).addOnSuccessListener(new OnSuccessListener<Void>() {
-//                                @Override
-//                                public void onSuccess(Void unused) {
-//                                    Log.i("Firebase Update", "User liked a post");
-//                                    User updatedUser = UserUtils.updateLikes(hike.getTitle(), user);
-//                                    user = SharedPrefUtils.saveUserInSP(updatedUser, getApplicationContext());
-//                                    runOnUiThread(new Runnable() {
-//                                        @Override
-//                                        public void run() {
-//                                            Toast.makeText(getApplicationContext(), "Marked as Favorite", Toast.LENGTH_SHORT).show();
-//                                            setLikeButtonColor(likeButton, true);
-//                                        }
-//                                    });
-//                                }
-//                            });
-//                        } else {
-//                            FirebaseApi.saveUserDislike(hike.getTitle(), user).addOnSuccessListener(new OnSuccessListener<Void>() {
-//                                @Override
-//                                public void onSuccess(Void unused) {
-//                                    Log.i("Firebase Update", "User unliked a post");
-//                                    User updatedUser = UserUtils.updateDislikes(hike.getTitle(), user);
-//                                    user = SharedPrefUtils.saveUserInSP(updatedUser, getApplicationContext());
-//                                    runOnUiThread(new Runnable() {
-//                                        @Override
-//                                        public void run() {
-//                                            Toast.makeText(getApplicationContext(), "Removed from Favorites", Toast.LENGTH_SHORT).show();
-//                                            setLikeButtonColor(likeButton, false);
-//                                        }
-//                                    });
-//                                }
-//                            });
-//                        }
-//                    }
-//                }
-//            });
-//
         }
     }
 
-    private void createHorizontalCard(HikeSerializable hike, String[] allImageURL, String[] allComments) {
+    private void createHorizontalCard(HikeSerializable hike,  List<CommentSerializable> commentSerializable) {
+        LatLangSerializable firstPoint = hike.getPath().get(0);
+        ArrayList<String> allImageURL = new ArrayList();
+        ArrayList<String> allComments = new ArrayList();
+        List<LatLangSerializable> allGeoPoints = hike.getPath();
+
+//        allImageURL.addAll(Arrays.asList("https://destinationlesstravel.com/wp-content/uploads/2019/05/DSC_9600-1-1024x684-1024x684.jpg","https://destinationlesstravel.com/wp-content/uploads/2020/04/Ella-train.jpg.webp"));
+//        allComments.addAll(Arrays.asList("asd","qwe"));
+//        allGeoPoints.addAll(Arrays.asList(null,null));
+        allComments.addAll(Collections.nCopies(path.size(), ""));
+        allImageURL.addAll(Collections.nCopies(path.size(), null));
+
         LinearLayout linearLayout = findViewById(R.id.horizontal_linear);
         linearLayout.removeAllViews();
-        for (int i=0;i<allImageURL.length;i++) {
-            linearLayout.addView(horizontalCard(allImageURL[i], allComments[i], hike, linearLayout));
+        for (int i=0;i<commentSerializable.size();i++) {
+            GeoPoint commentGeoPoint = commentSerializable.get(i).getLocation();
+            LatLangSerializable commentLatLang = new LatLangSerializable(commentGeoPoint.getLatitude(), commentGeoPoint.getLongitude());
+            for (int j=0;j<allGeoPoints.size();j++) {
+                if (Utils.getDistance(commentLatLang, allGeoPoints.get(j)) < 10) {
+                    if(allComments.get(j) == "") {
+                        allComments.set(j, commentSerializable.get(i).getComment());
+                        allImageURL.set(j, commentSerializable.get(i).getImageUrl());
+                        break;
+                    }
+                }
+            }
+        }
+        double totalDistance = Utils.getHikeDistance(allGeoPoints);
+        for (int i=0;i<hike.getPath().size();i++) {
+            double completedDistance = Utils.getHikeDistance(allGeoPoints.subList(0, i+1));
+//          completedDistance to string
+            linearLayout.addView(horizontalCard(allImageURL.get(i), allComments.get(i),allGeoPoints.get(i) ,new DecimalFormat("#.0#").format(completedDistance),new DecimalFormat("#.0#").format(totalDistance), hike, linearLayout));
         }
     }
 
-    private View horizontalCard(String url, String comment, HikeSerializable hike, LinearLayout mainLayout) {
+    private View horizontalCard(String url, String comment, LatLangSerializable geoPoint, String CompletedDistance , String TotalDistance, HikeSerializable hike, LinearLayout mainLayout) {
         LayoutInflater inflater = getLayoutInflater();
         View myLayout = inflater.inflate(R.layout.layout_blog_card, mainLayout, false);
         TextView title = myLayout.findViewById(R.id.title);
         TextView distance = myLayout.findViewById(R.id.distance);
         ImageView img = myLayout.findViewById(R.id.image);
         ImageView likeButton = myLayout.findViewById(R.id.likeButton);
+        TextView tags   = myLayout.findViewById(R.id.tags);
+        CardView cardView = myLayout.findViewById(R.id.card_view);
 
-        title.setText(hike.getTitle());
-        distance.setText(comment);
+        tags.setText(hike.getTitle());
+        title.setText(comment);
+
+        distance.setText("Remaining"+ " " + CompletedDistance + "/" + TotalDistance + "m");
         setLikeButtonColor(likeButton, UserUtils.userFav(user, hike.getTitle()));
+        likeButton.setVisibility(View.GONE);
 
         // set image
-        new ImageLoadTask(url, img).execute();
+        if (url == null) {
+            cardView.setVisibility(View.GONE);
+        }else {
+            new ImageLoadTask(url, img).execute();
+        }
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (user != null) {
+                    if (!UserUtils.userFav(user, hike.getTitle())) {
+                        FirebaseApi.saveUserLike(hike.getTitle(), user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.i("Firebase Update", "User liked a post");
+                                User updatedUser = UserUtils.updateLikes(hike.getTitle(), user);
+                                user = SharedPrefUtils.saveUserInSP(updatedUser, getApplicationContext());
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "Marked as Favorite", Toast.LENGTH_SHORT).show();
+                                        setLikeButtonColor(likeButton, true);
+                                    }
+                                });
+                            }
+                        });
+                    } else {
+                        FirebaseApi.saveUserDislike(hike.getTitle(), user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.i("Firebase Update", "User unliked a post");
+                                User updatedUser = UserUtils.updateDislikes(hike.getTitle(), user);
+                                user = SharedPrefUtils.saveUserInSP(updatedUser, getApplicationContext());
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getApplicationContext(), "Removed from Favorites", Toast.LENGTH_SHORT).show();
+                                        setLikeButtonColor(likeButton, false);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                }
+            }
+        });
 
+        myLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Clicked on " + comment , Toast.LENGTH_SHORT).show();
+                Log.i("Clicked on", comment + " " + geoPoint.getLatitude() + " " + geoPoint.getLongitude());
+                if (geoPoint != null) {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(geoPoint.getLatitude(), geoPoint.getLongitude()), ZOOM_LEVEL+5));
+                }
+            }
+        });
         return myLayout;
     }
 
@@ -176,8 +225,9 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
     @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
         try {
-            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json));
+            boolean success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json));
             if (!success) {
                 Log.e("HikePath", "Style parsing failed.");
             }
@@ -185,13 +235,13 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.e("HikePath", "Can't find style. Error: ", e);
         }
 
-        Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+        Polyline polyline1 = mMap.addPolyline(new PolylineOptions()
                 .clickable(true)
                 .addAll(path));
         stylePolyline(polyline1);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(path.get(0), ZOOM_LEVEL));
-        googleMap.setMyLocationEnabled(true);
-        googleMap.setOnPolylineClickListener(this);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(path.get(0), ZOOM_LEVEL));
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnPolylineClickListener(this);
     }
 
     @Override
@@ -225,7 +275,10 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
                 if (task.isSuccessful()) {
                     QuerySnapshot queryDocumentSnapshots = task.getResult();
                     commentSerializable = queryDocumentSnapshots.toObjects(CommentSerializable.class);
-
+                    for (CommentSerializable comment : commentSerializable) {
+                        Log.i("Comment", comment.getComment() + " "+ comment.getLocation().getLatitude()+ " "+ comment.getLocation().getLongitude()+" " + comment.getImageUrl());
+                    }
+                    Log.i("Comment", "Success");
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -234,6 +287,7 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
                     });
                 }
                 else {
+                    commentSerializable= new ArrayList<>();
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -241,6 +295,7 @@ public class BlogActivity extends AppCompatActivity implements OnMapReadyCallbac
                         }
                     });
                 }
+                createHorizontalCard(hike,commentSerializable);
             }
         });
     }
